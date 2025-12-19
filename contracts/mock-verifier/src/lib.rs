10181123fde06e756657c1b237f13b6d2006617f
Matteo Lisotto <matteo.lisotto@gmail.com>
2025-12-19 15:34:49 +0100

    feat: implement `RiscZeroVerifierInterface`

--- contracts/mock-verifier/src/lib.rs
+++ contracts/mock-verifier/src/lib.rs
@@ -2,7 +2,7 @@
 
 use soroban_sdk::{Bytes, BytesN, Env, contract, contractimpl};
 
-use risc0_interface::RiscZeroVerifierInterface;
+use risc0_interface::{Receipt, ReceiptClaim, RiscZeroVerifierInterface};
 
 const SELECTOR: [u8; 4] = [0, 0, 0, 0];
 
@@ -21,12 +21,35 @@ pub struct RiscZeroMockVerifier;
 #[contractimpl]
 impl RiscZeroMockVerifier {}
 
+#[contractimpl]
 impl RiscZeroVerifierInterface for RiscZeroMockVerifier {
     type Proof = ();
 
     fn verify(env: Env, seal: Bytes, image_id: BytesN<32>, journal: BytesN<32>) {
-        todo!()
+        let claim = ReceiptClaim::new(&env, image_id, journal);
+        let receipt = Receipt {
+            seal,
+            claim_digest: claim.digest(&env),
+        };
+
+        Self::verify_integrity(env, receipt);
     }
 
-    fn verify_integrity(env: Env, receipt: risc0_interface::Receipt) {}
+    fn verify_integrity(env: Env, receipt: risc0_interface::Receipt) {
+        let selector = receipt.seal.slice(0..4);
+
+        if selector != Bytes::from_array(&env, &SELECTOR) {
+            panic!("");
+        }
+
+        let selector_hash = env.crypto().keccak256(&selector).to_bytes();
+        let claim_hash = env
+            .crypto()
+            .keccak256(&receipt.claim_digest.into())
+            .to_bytes();
+
+        if selector_hash != claim_hash {
+            panic!("")
+        }
+    }
 }
